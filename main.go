e1759aaec7379f7033b88955757e5b320e23310f
Oksana <a1@MacBook-1.local>
2021-11-23 10:43:08 +0200

    shuffle func added

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
+// import (
+// 	"fmt"
+// )
+
 func main() {
-
-cards := deck{"Ace of Diamonds", newCard()}
-cards = append(cards, "Six of Spades")
-
+	cards := newDeck()
+	cards.shuffle()
 cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
